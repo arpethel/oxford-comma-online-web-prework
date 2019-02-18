d23878fe3e00977bd4886be6ab99de4c1bfece9b
Ryan Pethel <arpethel@gmail.com>
2019-02-18 16:52:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,6 @@ def oxford_comma(array)
     return array.join
   elsif array.length == 2
     array.join(" and ")
-  # elsif array.length == 3
-  #   last_ele = array.pop
-  #   arr = array.join(", ")
-  #   arr.concat(", and " + last_ele)
   else
     last_ele = array.pop
     arr = array.join(", ")
